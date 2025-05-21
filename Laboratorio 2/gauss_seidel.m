d8240993d0e2dc732ec02420c2e94b6bf9b3a1d6
Nmaurii <144194768+Nmaurii@users.noreply.github.com>
2025-05-21 19:52:14 -0400

    Create gauss_seidel.m

---
+++ Laboratorio 2/gauss_seidel.m
@@ -0,0 +1,18 @@
+clc;
+
+A = [ 10 -1 2 0
+     -1 11 -1 3
+      2 -1 10 -1
+      0 3 -1 8
+    ];
+
+L = tril(A,-1);
+U = triu(A,1);
+M = diag(diag(A))+L;
+N = -U;
+
+disp(M-N);
+
+T = inv(M)*N;
+
+disp(max(abs(eig(T))));
